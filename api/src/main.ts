d8c407961569194d6ef0503edb1000cef8fe2192
Carmen Araya Rodríguez <carmenarayarodriguez@gmail.com>
2024-06-12 00:16:56 -0400

    Agrega cambios para integración con front

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Habilita CORS para todas las rutas y todos los orígenes
   await app.listen(3000);
 }
 bootstrap();
